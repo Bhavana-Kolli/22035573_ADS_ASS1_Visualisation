ba917967e651d0e521977c60c7a2f2de8d598435
Bhavana-Kolli <125909616+Bhavana-Kolli@users.noreply.github.com>
2023-03-03 14:28:57 +0000

    Add files via upload

---
+++ 22035573_ADS_Ass_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+7PAM2000 Applied Data Science 1
+Assignment 1: Visualisation
+
+@author: Bhavana Kolli - 22035573
+"""
+
+# Here modules are imported
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Here functions for all plots are defined
+
+
