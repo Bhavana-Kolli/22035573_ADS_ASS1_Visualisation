549a7689334e40a0863c2d3d38128768e434343b
Bhavana-Kolli <125909616+Bhavana-Kolli@users.noreply.github.com>
2023-03-04 15:59:20 +0000

    Update 22035573_ADS_Ass_1.py

--- 22035573_ADS_Ass_1.py
+++ 22035573_ADS_Ass_1.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 
 # Here functions for all plots are defined
 
+
 def filter_data(data, country):
     """
     Filter the data to only include matches where the specified country 
@@ -31,26 +32,29 @@ def filter_data(data, country):
     # Extract year from date and create new column.
     filtered_data['year'] = pd.to_datetime(filtered_data['date']).dt.year
     # Aggregates the goals scored by year
-    filtered_data = filtered_data.groupby('year').agg({'home_score': 'sum'}).reset_index()
+    filtered_data = filtered_data.groupby('year').agg({'home_score': 
+                                                       'sum'}).reset_index()
     return filtered_data
 
 
 def create_line_plot(data, countries):
     """
-    Create a line plot of the number of goals scored by each country over the years.
+    Create a line plot of the number of goals scored 
+    by each country over the years.
 
     Args:
         data(pandas.DataFrame): The input data.
-        countries (list of str): The names of the countries to include in the plot.
+        countries(list of str): The names of countries to include in plot.
     """
     # Plot the line plot for countries in the string
+    plt.figure()
     for country in countries:
         filtered_data = filter_data(data, country)
-        plt.plot(filtered_data['year'], filtered_data['home_score'], label=country)
+        plt.plot(filtered_data['year'], filtered_data['home_score'], 
+                 label=country)
     
     # set the upper and lower limits of x
     plt.xlim(1900, 2020)
-    
     # Set labels, title to the plot.
     plt.xlabel('Years')
     plt.ylabel('Goals Scored')
@@ -60,13 +64,60 @@ def create_line_plot(data, countries):
     plt.show()
 
 
+def plot_histogram(data, variable):
+    """
+    Create a histogram of a given variable in a DataFrame.
+    
+    Args:
+        data(pandas.DataFrame): The input data.
+        variable (str): The name of the variable to create a histogram for.
+    """
+    
+    # Plot the distribution as a histogram with 30 bins
+    plt.figure(figsize=(8,6))
+    plt.hist(data[variable], bins=30, label="medv", density=True, 
+             alpha=0.7, color='orange')
+    # Set labels, title to the plot.
+    plt.title("Distribution of Median Home Values in Boston")
+    plt.xlabel("Median value of owner-occupied homes in $1000s")
+    plt.ylabel("Frequency")
+    # show legend
+    plt.legend()
+    plt.show()
+    
+
 # Main Program
 
+
+# PLOT-1 (LINE PLOT)-------------------------------------------------------
+
+
 # read CSV file into dataframe and print
-data = pd.read_csv("results.csv")
-print(data)
+df_data_lp = pd.read_csv("results.csv")
+print(df_data_lp)
+
 # Countries for which we have plot lineplot
 countries = ['Brazil', 'England', 'Spain']
-# Call the above function for plotting
-create_line_plot(data, countries)
+
+# Call the create_line_plot function for the data
+create_line_plot(df_data_lp, countries)
+
+
+# PLOT-2 (HISTOGRAM)-------------------------------------------------------
+
+
+# Load the dataset from the URL into dataframe
+url = 'https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv'
+df_data_hp = pd.read_csv(url)
+
+# Print the dataframe
+print(df_data_hp)
+
+# Extract 'medv' variable and print it
+df_medv = df_data_hp['medv']
+print("Median value of owner-occupied homes in $1000s in Boston")
+print(df_medv)
+
+# Call the plot_histogram function for 'medv' variable in dataset
+plot_histogram(df_data_hp, 'medv')
 
